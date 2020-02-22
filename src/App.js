a60fa7692f9ec1ba6b0478430ca47bb287ae7e87
claire-codes <claire-codes@users.noreply.github.com>
2020-02-22 22:15:04 +0000

    Adds Delete Todo button and function

--- src/App.js
+++ src/App.js
@@ -11,13 +11,26 @@ class App extends React.Component {
     todos: ["alpha", "beta", "gamma"]
   };
 
+  deleteTodo = index => {
+    let newTodos = [...this.state.todos];
+    newTodos.splice(index, 1);
+    this.setState({ todos: newTodos });
+  };
+
   render() {
     return (
       <MainBody>
         <h1>To-Do List</h1>
         <ul>
-          {this.state.todos.map(item => {
-            return <li>{item}</li>;
+          {this.state.todos.map((item, index) => {
+            return (
+              <li key={index}>
+                {item}&nbsp;
+                <button type="button" onClick={() => this.deleteTodo(index)}>
+                  Delete
+                </button>
+              </li>
+            );
           })}
         </ul>
       </MainBody>
