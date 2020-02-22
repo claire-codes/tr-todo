87e1ff5e8223b38bcaccc928cfa50a0a60aed716
claire-codes <claire-codes@users.noreply.github.com>
2020-02-22 22:05:19 +0000

    Renders list items from local state

--- src/App.js
+++ src/App.js
@@ -7,10 +7,19 @@ const MainBody = styled.div`
 `;
 
 class App extends React.Component {
+  state = {
+    todos: ["alpha", "beta", "gamma"]
+  };
+
   render() {
     return (
       <MainBody>
         <h1>To-Do List</h1>
+        <ul>
+          {this.state.todos.map(item => {
+            return <li>{item}</li>;
+          })}
+        </ul>
       </MainBody>
     );
   }
