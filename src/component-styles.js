c04b35c5ce2db42165a3618d9d55f95f8d7de0e0
claire-codes <claire-codes@users.noreply.github.com>
2020-02-23 22:25:05 +0000

    Moves styled components into own file

---
+++ src/component-styles.js
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+
+const AppBody = styled.main`
+  height: 100vh;
+  margin-left: 50px;
+  font-size: 40px;
+  border-left: 2px solid #e7847e;
+  padding-left: 20px;
+`;
+
+const TodoItemText = styled.span`
+  text-decoration: ${props => (props.complete ? "line-through" : "none")};
+  color: ${props => (props.complete ? "#aaa" : "inherit")};
+`;
+
+const Heading = styled.header`
+  border-bottom: 2px solid #7aafff;
+  margin-left: -70px;
+  padding-left: 70px;
+  padding-top: 40px;
+`;
+
+const AppTitle = styled.h1`
+  margin: 0;
+  font-size: 50px;
+`;
+
+const TodoInput = styled.input`
+  font-size: 40px;
+  font-family: "Learning Curve Pro";
+  border: none;
+  border-bottom: 1px solid black;
+  background-color: unset;
+  width: 350px;
+  padding-left: 10px;
+`;
+
+const TodoList = styled.ul`
+  margin: 0;
+`;
+
+const InputContainer = styled.div`
+  margin: 20px 0;
+`;
+
+const Button = styled.button`
+  margin-left: 15px;
+  font-size: 30px;
+  background-color: unset;
+  border-radius: 5px;
+  font-family: "Learning Curve Pro";
+  padding: 3px 15px;
+  box-shadow: 1px 1px 2px #ccc;
+  border-color: #ccc;
+`;
+
+export {
+  AppBody,
+  TodoItemText,
+  Heading,
+  AppTitle,
+  TodoInput,
+  TodoList,
+  InputContainer,
+  Button
+};
