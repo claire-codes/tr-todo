75b8de13cb9091eeead44359a098bedd69fc464c
claire-codes <claire-codes@users.noreply.github.com>
2020-02-24 14:06:31 +0000

    Destructures props in component

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,12 +2,15 @@ import React from "react";
 
 import { StyledButton } from "./../component-styles.js";
 
-function Button(props) {
-  let buttonType = props.buttonType || "button";
-  let buttonText = props.buttonText || "Button";
-
+function Button({
+  buttonText = "Click me",
+  buttonType = "button",
+  clickEvent = () => {
+    return;
+  }
+}) {
   return (
-    <StyledButton type={buttonType} onClick={props.clickEvent}>
+    <StyledButton type={buttonType} onClick={clickEvent}>
       {buttonText}
     </StyledButton>
   );
